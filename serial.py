0b2a0b0c0f215d902dfb2a98897cc2636473d1a5
TechEdDan2 <dg.nelson2@gmail.com>
2024-09-23 19:15:36 -0400

    implimented the constructor and other required methods to produce the correct output for the tests

--- serial.py
+++ serial.py
@@ -20,3 +20,22 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        """ Initialize a new SerialGenerator, starting at the start"""
+        self.start = start
+        self.count = self.start
+
+    def __repr__(self):
+        """ Display the representation of the object """
+        return f"<SerialGenerator start={self.start} count={self.count}"
+
+    def generate(self):
+        """ Generate returns the next number in the increased count """
+        self.count += 1
+        return self.count
+
+    def reset(self):
+        """ Return to the starting number """
+        self.count = self.start
+
+
